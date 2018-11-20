e65bcb62cba3e5210bad6daa6033fb8b3cd1a498
Kravchuk <majerkravchuk@gmail.com>
2018-11-21 01:22:42 +0300

    Reorder columns on users table

--- src/components/Matches/UsersList.vue
+++ src/components/Matches/UsersList.vue
@@ -2,8 +2,8 @@
   <div class="users">
     <el-table :data="users" style="width: 100%">
       <el-table-column align="center" prop="steamId" label="SteamId"></el-table-column>
-      <el-table-column align="center" prop="team" label="Team"></el-table-column>
       <el-table-column align="center" prop="frags" label="Frags"></el-table-column>
+      <el-table-column align="center" prop="team" label="Team"></el-table-column>
       <el-table-column align="center" label="Is Winner">
         <template slot-scope="scope">
           <el-tag v-if="scope.row.isWinner" type="success">Yes</el-tag>
