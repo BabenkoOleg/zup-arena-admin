d723461d0a72ffcaaf5cd4d9e7292c75eba949f9
Kravchuk <majerkravchuk@gmail.com>
2018-12-02 01:46:12 +0300

    Disable consistent-return rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-param-reassign': 'off',
+    'consistent-return': 'off',
     'no-return-assign': 'off',
     'no-unused-expressions': 'off',
     'import/no-unresolved': 'off',
