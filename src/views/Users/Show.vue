defce9c93ccd10bd8e9ff28a31a88a29745013c7
Kravchuk <majerkravchuk@gmail.com>
2018-12-10 17:58:56 +0300

    Add frags info to user page

--- src/views/Users/Show.vue
+++ src/views/Users/Show.vue
@@ -8,54 +8,87 @@
                    @click="refresh">Refresh</el-button>
       </div>
       <div v-if="user" class="user-info">
-        <el-row :gutter="20">
-          <el-col :span="6">
-            <el-card class="box-card">
-              <div slot="header" class="clearfix">
-                <span>Progress</span>
-              </div>
-              <p>Level: <span class="field">{{ user.level }}</span></p>
-              <p>Rank: <span class="field">{{ user.rank }}</span></p>
-              <p>XP: <span class="field">{{ user.xp }}</span></p>
-              <p>Money: <span class="field">{{ user.money }}</span></p>
-            </el-card>
-          </el-col>
-
-          <el-col :span="6">
-            <el-card class="box-card">
-              <div slot="header" class="clearfix">
-                <span>Matches</span>
-              </div>
-              <p>Total: <span class="field">{{ user.matches.total }}</span></p>
-              <p>Victories: <span class="field">{{ user.matches.victories }}</span></p>
-              <p>Defeats: <span class="field">{{ user.matches.defeats }}</span></p>
-              <p>Winning percentage: <span class="field">{{ user.matches.winningPercentage }}%</span></p>
-            </el-card>
-          </el-col>
-
-          <el-col :span="12">
-            <el-card class="box-card steam-info">
-              <div slot="header" class="clearfix">
-                <span>Info</span>
-              </div>
-              <div class="steam-info-fields">
-                <p>Id: <span class="field">{{ user.steamId }}</span></p>
-                <p>Name: <span class="field">{{ user.steamName }}</span></p>
-                <p>CountryCode: <span class="field">{{ user.steamCountryCode }}</span></p>
-                <p>
-                  Banned:
-                  <el-switch v-model="user.banned"
-                             active-color="#ff4949"
-                             active-text="Yes!"
-                             inactive-text="No"
-                             @change="changeBanned">
-                  </el-switch>
-                </p>
-              </div>
+        <el-row>
+          <el-col :span="24">
+            <div class="steam-info">
               <div class="avatar">
                 <img v-if="user.steamAvatar" :src="user.steamAvatar" alt="">
                 <img v-else src="../../assets/images/default-avatar.jpg" alt="">
               </div>
+              <div class="steam-info-fields">
+                <p>
+                  Id: <span class="field">{{ user.steamId }}</span>
+                </p>
+                <p>
+                  Name: <span class="field">{{ user.steamName }}</span>
+                </p>
+                <p>
+                  CountryCode: <span class="field">{{ user.steamCountryCode }}</span>
+                </p>
+                <p>
+                  Banned:
+                  <el-switch v-model="user.banned"
+                              active-color="#ff4949"
+                              active-text="Yes!"
+                              inactive-text="No"
+                              @change="changeBanned">
+                  </el-switch>
+                </p>
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <el-card class="box-card">
+              <div slot="header" class="clearfix">
+                <span>Progress</span>
+              </div>
+              <p>
+                Level: <span class="field">{{ user.level }}</span>
+              </p>
+              <p>
+                Rank: <span class="field">{{ user.rank }}</span>
+              </p>
+              <p>
+                XP: <span class="field">{{ user.xp }}</span>
+              </p>
+              <p>
+                Money: <span class="field">{{ user.money }}</span>
+              </p>
+            </el-card>
+          </el-col>
+
+          <el-col :span="8">
+            <el-card class="box-card">
+              <div slot="header" class="clearfix">
+                <span>Matches</span>
+              </div>
+              <p>
+                Total: <span class="field">{{ user.matches.total }}</span>
+              </p>
+              <p>
+                Victories: <span class="field">{{ user.matches.victories }}</span>
+              </p>
+              <p>
+                Defeats: <span class="field">{{ user.matches.defeats }}</span>
+              </p>
+              <p>
+                Winning percentage: <span class="field">{{ user.matches.winningPercentage }}%</span>
+              </p>
+            </el-card>
+          </el-col>
+
+          <el-col :span="8">
+            <el-card class="box-card">
+              <div slot="header" class="clearfix">
+                <span>Frags</span>
+              </div>
+              <p>Total: <span class="field">{{ user.frags.total }}</span></p>
+              <p>Enemies: <span class="field">{{ user.frags.enemies }}</span></p>
+              <p>Teammates: <span class="field">{{ user.frags.teammates }}</span></p>
+              <p>Suicides: <span class="field">{{ user.frags.suicides }}</span></p>
             </el-card>
           </el-col>
         </el-row>
@@ -88,7 +121,7 @@ export default {
     changeBanned(value) {
       this.isLoading = true;
       this[actionTypes.UPDATE]({ id: this.user.steamId, params: { banned: value } })
-        .then((user) => {
+        .then(() => {
           this.user.banned = value;
           this.isLoading = false;
         });
@@ -110,6 +143,19 @@ export default {
     .el-row {
       margin-bottom: 20px;
 
+      .steam-info {
+        .avatar {
+          img {
+            box-shadow: 0 2px 12px 0 #00000085;
+          }
+        }
+
+        .steam-info-fields {
+          flex-grow: 1;
+          text-align: center;
+        }
+      }
+
       /deep/ .el-card {
         box-shadow: 0 2px 12px 0 #00000054;
         height: 100%;
@@ -119,17 +165,6 @@ export default {
           color: #3d464d;
           font-weight: 600;
           padding: 10px;
-        }
-
-        &.steam-info {
-          .el-card__body {
-            align-items: center;
-            display: flex;
-
-            .steam-info-fields {
-              flex-grow: 1;
-            }
-          }
         }
       }
     }
